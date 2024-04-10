f7702bf202fb614b9a31b06b76d3f699232679fc
lachezar-atanasov <lachezar.atanasov99@gmail.com>
2024-04-10 11:35:55 +0300

    Add project files.

---
+++ Cosmetics/Models/Toothpaste.cs
@@ -0,0 +1,19 @@
+﻿using Cosmetics.Models.Enums;
+using System;
+
+namespace Cosmetics.Models
+{
+    public class Toothpaste
+    {
+        private const int NameMinLength = 3;
+        private const int NameMaxLength = 10;
+        private const int BrandMinLength = 2;
+        private const int BrandManLength = 10;
+
+        public Toothpaste(string name, string brand, decimal price, GenderType gender, string ingredients)
+        {
+            throw new NotImplementedException("Not implemented yet.");
+        }
+
+    }
+}
